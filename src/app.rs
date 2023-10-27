39220ea53eff2d8ca2c496f8dae80e784ae1162d
renpet-hhh <renandcsc@gmail.com>
2023-10-27 15:52:41 -0300

    Enhance UI with Stack layout

--- src/app.rs
+++ src/app.rs
@@ -1,23 +1,24 @@
-
-use web_sys::{HtmlInputElement, HtmlTextAreaElement};
+use web_sys::{File, HtmlTextAreaElement};
 use yew::prelude::*;
 
 mod af;
+mod components;
 mod glue;
 mod graph;
+mod layout;
 mod sat;
 pub(crate) mod util;
 
 use crate::app::{
     af::{encoding::Enconding, semantics::Semantics, AF},
+    components::file_input::FileInput,
     graph::VisDrawable,
+    layout::Stack,
     util::read_file,
 };
 
-
 #[function_component(App)]
 pub fn app() -> Html {
-    let input_ref = use_node_ref();
     let textarea_ref = use_node_ref();
     let af_text_handle = use_state(|| String::from(""));
     let af_text = (*af_text_handle).clone();
@@ -27,31 +28,25 @@ pub fn app() -> Html {
     let complete = framework.complete();
 
     let load_af = {
-        let input_ref = input_ref.clone();
         let af_text_handle = af_text_handle.clone();
         let vis_page = vis_page.clone();
-        Callback::from(move |_| {
+        Callback::from(move |f: File| {
             vis_page.set(0);
-            // get the uploaded file
-            let input: Option<HtmlInputElement> = input_ref.cast::<HtmlInputElement>();
-            let file = input.and_then(|x| x.files()).and_then(|x| x.get(0));
-            if let Some(file) = file {
-                let af_text_handle = af_text_handle.clone();
-                // asynchronously read the file and update the framework
-                wasm_bindgen_futures::spawn_local(async move {
-                    let text = read_file(file).await;
-                    if let Some(text) = text {
-                        af_text_handle.set(text);
-                    }
-                });
-            }
+            let af_text_handle = af_text_handle.clone();
+            // asynchronously read the file and update the framework
+            wasm_bindgen_futures::spawn_local(async move {
+                let text = read_file(f).await;
+                if let Some(text) = text {
+                    af_text_handle.set(text);
+                }
+            });
         })
     };
 
     let handle_af_text_change = {
         let af_text_handle = af_text_handle.clone();
         let textarea_ref = textarea_ref.clone();
-        Callback::from(move |_| {
+        Callback::from(move |_: Event| {
             if let Some(textarea) = textarea_ref.cast::<HtmlTextAreaElement>() {
                 af_text_handle.set(textarea.value());
             }
@@ -60,7 +55,7 @@ pub fn app() -> Html {
 
     let prev_page = {
         let vis_page = vis_page.clone();
-        Callback::from(move |_| {
+        Callback::from(move |_: MouseEvent| {
             if *vis_page > 0 {
                 vis_page.set(*vis_page - 1);
             }
@@ -70,25 +65,35 @@ pub fn app() -> Html {
     let next_page = {
         let vis_page = vis_page.clone();
         let num_of_pages = complete.len();
-        Callback::from(move |_| {
+        Callback::from(move |_: MouseEvent| {
             if *vis_page < num_of_pages - 1 {
                 vis_page.set(*vis_page + 1);
             }
         })
     };
 
+    // Synchronize the network visualization
     framework.update_vis("af-graph", complete.get(*vis_page));
+
     html! {
-        <main>
-            <input ref={input_ref} type={"file"} onchange={load_af}/>
-            <textarea ref={textarea_ref} value={af_text} onchange={handle_af_text_change} />
-            <button onclick={prev_page}>{ "Previous" }</button>
-            <button onclick={next_page}>{ "Next" }</button>
-            <div style="border: 2px solid black;width:512px;" id="af-graph"></div>
+        <Stack vertical={true}>
+            <Stack align_items="center">
+                <Stack vertical={true}>
+                    <FileInput id="load-af" text="Load AF" onload={load_af} />
+                    <textarea style="width: 512px; height: 512px;" ref={textarea_ref} value={af_text} onchange={handle_af_text_change} />
+                </Stack>
+                <Stack vertical={true}>
+                    <Stack>
+                        <button onclick={prev_page}>{ "Previous" }</button>
+                        <button onclick={next_page}>{ "Next" }</button>
+                    </Stack>
+                    <div style="border: 2px solid black;width:512px;height:512px;" id="af-graph"></div>
+                </Stack>
+            </Stack>
             <p>{ format!("AF: {:?}", framework) }</p>
             <p>{ format!("Complete: {:?}", complete) }</p>
             <p>{ format!("Stable: {:?}", framework.stable()) }</p>
             <p>{ format!("Preferred: {:?}", framework.preferred()) }</p>
-        </main>
+        </Stack>
     }
 }
---
+++ src/app/components/file_input.rs
@@ -0,0 +1,44 @@
+use web_sys::{File, HtmlInputElement};
+use yew::prelude::*;
+
+#[derive(Properties, PartialEq)]
+pub struct FileInputProps {
+    pub id: String,
+    #[prop_or_default]
+    pub text: String,
+    pub onload: Callback<File>,
+}
+
+#[function_component]
+pub fn FileInput(props: &FileInputProps) -> Html {
+
+    let input_ref = use_node_ref();
+
+    let handle_change = {
+        let input_ref = input_ref.clone();
+        let onload = props.onload.clone();
+        Callback::from(move |_: Event| {
+            // get first file from <input />
+            let file = input_ref
+                .cast::<HtmlInputElement>()
+                .and_then(|x| x.files())
+                .and_then(|x| x.get(0));
+            if let Some(file) = file {
+                onload.emit(file);
+            }
+        })
+    };
+
+    html! {
+        <>
+            <input
+                id={props.id.clone()}
+                ref={input_ref}
+                type="file"
+                onchange={handle_change}
+                style="display: none;"
+            />
+            <label for={props.id.clone()}>{ props.text.clone() }</label>
+        </>
+    }
+}
